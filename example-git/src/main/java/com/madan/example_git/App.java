a7973be310b57943d31edd4b6ee9584f15c853d8
madan <madan.sunkara@gmail.com>
2021-07-15 19:28:07 +0530

    first commit

---
+++ example-git/src/main/java/com/madan/example_git/App.java
@@ -0,0 +1,14 @@
+package com.madan.example_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("------Hello World Again---------------");
+    }
+}
---
+++ example-git/src/main/java/com/madan/example_git/MyName.java
@@ -0,0 +1,8 @@
+package com.madan.example_git;
+
+public class MyName {
+	public static void main(String[] args) {
+		System.out.println("--------My Name--------------");
+	}
+
+}
